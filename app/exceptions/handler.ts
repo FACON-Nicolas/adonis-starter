98623615e21ffcf781d062d2d8ecd23be55ec146
nicolas_facon <faconicolas@gmail.com>
2024-06-16 13:49:18 +0200

    fix(back): manage login errors

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,5 +1,7 @@
 import app from '@adonisjs/core/services/app'
 import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
+import { errors as authErrors } from '@adonisjs/auth'
+
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   /**
@@ -13,6 +15,18 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * response to the client
    */
   async handle(error: any, ctx: HttpContext) {
+    //return ctx.response.status(500).json({
+    //  error,
+    //})
+    if (error instanceof authErrors.E_UNAUTHORIZED_ACCESS) {
+      return ctx.response.status(401).json({
+        error: "Vous n'êtes pas autorisé à accéder à cette ressource",
+      })
+    } else if (error instanceof authErrors.E_INVALID_CREDENTIALS) {
+      return ctx.response.status(401).json({
+        error: "Les informations d'identification fournies sont incorrectes",
+      })
+    }
     return ctx.response.status(error.status).json({
       error: error.messages ? error.messages[0].message : error.message || 'An error occurred',
     })
